e1ce48a2ac26c5ab23b3cbee136224f3f8bbb9d4
nehakhatri2021 <110082217+nehakhatri2021@users.noreply.github.com>
2024-04-11 16:38:22 +0530

    app.py

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ st.markdown(
     """
     <style>
     .stApp {
-        max-width: 700px;
-        margin: 0 auto;
+        width: 100vw;
+        height: 100vh;
         color: white;
-        background-color: #008080; /* Background color */
     }
     .stTextInput>div>div>input {
         background-color: #f0f0f0;
-        color: black;  /* Keep the user input text as black */
+        color: black;
         font-size: 16px;
         padding: 12px 15px;
         border: 1px solid #ccc;
@@ -39,40 +38,41 @@ st.markdown(
     .stMarkdown {
         color: white;
     }
-    .footer {
-        background-color: #444444; /* Footer background color */
-        padding: 20px 0;
-        text-align: center;
-    }
-    .footer p {
-        color: white;
-        font-size: 16px;
-    }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-def main():
-    """
-    The main function to run the recipe information web app.
+def login():
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if username == "neha" and password == "1234":
+            st.success("Login successful!")
+            return True
+        else:
+            st.error("Invalid username or password")
+            return False
 
-    This function sets up the Streamlit app, connects to the Spoonacular API,
-    and handles user interactions to fetch and display recipe data.
-    """
+def main():
+    if not login():
+        return
+
     api_connection = SpoonacularConnectionProvider(connection_name='recipeProvider')
 
     st.title("Recipedia")
     st.markdown("Enter your desired food name and we'll find you a recipe! :)")
 
     recipes_input = st.text_input("Enter your preferred food choice")
+    
 
     if st.button("Get Recipes"):
         try:
             recipes = [city.strip() for city in recipes_input.split(",")]
             recipes_data = api_connection.query(recipes)
-            if(len(recipes_data) != 0):
+            if len(recipes_data) != 0:
                 st.success("Recipes fetched successfully!")
+                
                 display_recipes_data(recipes_data)
             else:
                 st.error("No recipes found for the given query.")    
@@ -92,8 +92,6 @@ def display_recipes_data(recipes_data):
             with st.expander("See Recipe Details"):
                 try:
                     recipe_data = api_connection.query(data['id'])
-
-                    # Display recipe details
                     st.image(recipe_data['image'])
                     st.markdown(f"## Price per Serving: {recipe_data['pricePerServing']}")
                     st.markdown(f"## Health Score: {recipe_data['healthScore']}")
@@ -105,23 +103,62 @@ def display_recipes_data(recipes_data):
                     st.markdown(f"## Ready in Minutes: {recipe_data['readyInMinutes']}")
                 except Exception as e:
                     st.error(f"Error occurred while fetching recipe details: {e}")
-
+                    
     st.markdown("---")
 
-def footer():
-    st.markdown(
-        """
-        <div class="footer">
-            <p><b>About Us:</b> Recipedia is your go-to platform for finding delicious recipes. We provide a wide range of recipes for every taste!</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
 
 def layout(*args):
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+    </style>
+    """
+
+    style_div = styles(
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        text_align="center",
+        height="60px",
+        opacity=0.6
+    )
+
+    style_hr = styles()
+
+    body = p()
+    foot = div(style=style_div)(hr(style=style_hr), body)
+
+    st.markdown(style, unsafe_allow_html=True)
+
     for arg in args:
-        if isinstance(arg, HtmlElement):
-            st.markdown(str(arg), unsafe_allow_html=True)
+        if isinstance(arg, str):
+            body(arg)
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+def footer():
+    myargs = [
+        "<b>Made with</b>: Python 3.9.6 ",
+        link("https://www.python.org/", image('https://i.imgur.com/ml09ccU.png',
+        	width=px(18), height=px(18), margin= "0em")),
+        ", Streamlit ",
+        link("https://streamlit.io/", image('https://res.cloudinary.com/dc0tfxkph/image/upload/v1690664263/pngaaa.com-5084798.png',
+        	width=px(24), height=px(25), margin= "0em")),
+        ", and ❤️ in India by Neha ",
+        link("https://github.com/Pavel401", image('https://res.cloudinary.com/dc0tfxkph/image/upload/v1690664339/47685150.jpg',
+        	width=px(24), height=px(25), margin= "0em", border_radius=px(50))),
+        br(),
+    ]
+    layout(*myargs)
 
 if __name__ == "__main__":
     main()
